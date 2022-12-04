c18d68d89688176a20acdc94108bfe48f1274edb
abdul-yusuf <abdul17yusuf@gmail.com>
2022-12-04 23:05:54 +0100

    added procfile

--- Nexus_v2/settings.py
+++ Nexus_v2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f$sc1e&uuf7fed=wi)2%mo$^skokyhni1*az64s7jjznf!whi$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn Nexus_v2.wsgi:application
