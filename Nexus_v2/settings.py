576c5714dc611aff085209a20d23210967acd97c
abdul-yusuf <abdul17yusuf@gmail.com>
2022-12-06 19:39:33 +0100

    requirements.txt updated

--- Nexus_v2/settings.py
+++ Nexus_v2/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-import environ
+# import environ
 import os
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+env = os.environ.get
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
