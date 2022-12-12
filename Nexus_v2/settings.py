b7dcae91eb6b078636bc512aa16901074554e06f
Yusuf Abdulhadi <61198189+abdul-yusuf@users.noreply.github.com>
2022-12-12 07:28:47 +0100

    Update settings.py

--- Nexus_v2/settings.py
+++ Nexus_v2/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# if env('LIVE')==1:
-#     DEBUG = True
-# else:
-DEBUG = False
+if env('LIVE')==1:
+    DEBUG = False
+else:
+    DEBUG = True 
 
 
 ALLOWED_HOSTS = ['*']
